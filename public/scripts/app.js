8fc0bc5f067de306b32b209c48c19d0814a9b856
froliceric0914 <wzhao.eric@gmail.com>
2018-11-19 15:50:09 +0000

    Add map point screen alert for presentation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,13 @@
-const TOKEN = 'sk.eyJ1Ijoic3JpLWRldiIsImEiOiJjam9obnhnYW4wMDl3M3Zucjh1NDJxbGZoIn0.WcfmfgzddXP4D-_JAAcAKQ';
-const ATTRIBUTION = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+const TOKEN =
+  "sk.eyJ1Ijoic3JpLWRldiIsImEiOiJjam9obnhnYW4wMDl3M3Zucjh1NDJxbGZoIn0.WcfmfgzddXP4D-_JAAcAKQ";
+const ATTRIBUTION =
+  'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
 $(() => {
   $.ajax({
     method: "GET",
     url: "/api/map/1/places"
-  }).done((places) => {
+  }).done(places => {
     // for (place of places) {
     //   console.log(place.place_lat, place.place_long);
     //   // $("<div>").text(user.name).appendTo($("body"));
@@ -14,10 +16,10 @@ $(() => {
     $.ajax({
       method: "GET",
       url: "/maps/all",
-      success: function (rows) {
+      success: function(rows) {
         createMapList(rows);
-      },
-    })
+      }
+    });
 
     let mapUI = createNewMap();
 
@@ -26,34 +28,35 @@ $(() => {
       createMarker(mapUI, place);
     }
 
-    $('#map-list ul').on("click", 'li', function () {
+    $("#map-list ul").on("click", "li", function() {
       const mapId = $(this).data("mapId");
       // alert("you clicked a map with map id = " + $(this).data("mapId"));
       $.ajax(`/maps/${mapId}`, {
         method: "GET",
-        success: function (places) {
+        success: function(places) {
           mapUI.remove();
           mapUI = createNewMap();
           for (const place of places) {
             createMarker(mapUI, place);
           }
-        },
-      })
+        }
+      });
     });
 
     function createNewMap() {
       let mapUI = L.map("mapid", {
         center: [43.6426, -79.3871],
-        zoom: 15,
+        zoom: 15
       });
 
       // add a tile layer to add to our map
       L.tileLayer(
-        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+        {
           attribution: ATTRIBUTION,
           maxZoom: 20,
           id: "mapbox.streets",
-          accessToken: TOKEN,
+          accessToken: TOKEN
         }
       ).addTo(mapUI);
 
@@ -61,33 +64,35 @@ $(() => {
     }
 
     function createMapList(mapList) {
-      $('#map-list ul li').remove();
+      $("#map-list ul li").remove();
       for (const map of mapList) {
-        const $listItem = $('<li>')
-          .addClass('spaced-list')
+        const $listItem = $("<li>")
+          .addClass("spaced-list")
           .append(`<a href="#">${map.name}</a>`)
           .data({
             mapId: map.id
           });
-        $('#map-list ul').append($listItem);
+        $("#map-list ul").append($listItem);
         // $('#map-list ul').append(`<li class="spaced-list"><a href="#">${map.name}</a></li>`);
       }
     }
 
     function createMarker(mapObj, place) {
-      const div = L.DomUtil.create('div', 'popup');
-      new L.Marker([place.place_lat, -place.place_long]).bindPopup(div).addTo(mapObj);
+      const div = L.DomUtil.create("div", "popup");
+      new L.Marker([place.place_lat, -place.place_long])
+        .bindPopup(div)
+        .addTo(mapObj);
 
-      const img = L.DomUtil.create('img', 'place-img', div);
+      const img = L.DomUtil.create("img", "place-img", div);
       img.src = `${place.image_url}`;
 
-      const title = L.DomUtil.create('a', 'place-title', div);
+      const title = L.DomUtil.create("a", "place-title", div);
       title.text = `${place.name}`;
       title.href = `${place.place_url}`;
       title.target = "_blank";
 
-      const desc = L.DomUtil.create('p', 'place-desc', div);
+      const desc = L.DomUtil.create("p", "place-desc", div);
       desc.textContent = `${place.description}`;
     }
-  });;
+  });
 });
